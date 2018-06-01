0fd2177d0f03008ff532e60418a161ec13326eaf
Aaron S. Jackson <aaron.jackson@nottingham.ac.uk>
2018-06-01 01:20:12 +0100

    Can create a cudnn object

--- cudnn.test.lisp
+++ cudnn.test.lisp
@@ -15,11 +15,11 @@
 	"Your version of CuDNN is"
 	(convert-from-foreign (cudnnGetVersion) :unsigned-int)))
 
+(defparameter *handle* (cffi:foreign-alloc :pointer))
+(princ (foreign-funcall "cudnnCreate" :pointer *handle*
+		 cudnnStatus_t))
 
+(princ *handle*) ;; should be CUDNN_STATUS_SUCCESS if you set
+;; CUDA_VISIBLE_DEVICES to something invalid it will return
+;; CUDNN_STATUS_NOT_INITIALIZED
 
-;;cudnnHandle_t hCudNN = NULL;
-;;cudnnTensor4dDescriptor_t pInputDesc = NULL;
-;;cudnnFilterDescriptor_t pFilterDesc = NULL;
-;;cudnnConvolutionDescriptor_t pConvDesc = NULL;
-;;cudnnTensor4dDescriptor_t pOutputDesc = NULL;
-;;cudnnStatus_t status;
